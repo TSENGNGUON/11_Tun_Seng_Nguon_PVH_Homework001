49f8c1a64577ca166b9920024ec1fbda70b04ae2
nguonn <sengn5112@gmail.com>
2025-08-03 15:18:17 +0700

    done homework 1

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -2,8 +2,8 @@ import kotlin.math.pow
 
 fun main(){
 //    Step 1 Define Products (Arrays/Lists)
-        val productNames = listOf("Laptop", "Mouse", "Keyboard", "Monitor", "Webcam")
-        val productPrices = listOf(999.99,25.50,45.40,189.99,49.99)
+        val productNames = listOf("Laptop", "Mouse", "Keyboard", "Monitor", "Webcam","Wireless Charger","USB Hub","Headphones","External Hard Drive","Smartphone","Tablet","Bluetooth Speaker","Printer","Graphics Card","Desk Lamp","Office Chair","Microphone")
+        val productPrices = listOf(999.99,25.50,45.40,189.99,49.99,29.99,15.00,89.99,79.50,699.99,399.99,59.99,149.99,349.99,24.99,129.99,25.99)
         val invalidProduct = mutableListOf<String>()
 
 
@@ -44,9 +44,6 @@ fun main(){
                    break
                }
 
-
-
-
            }
 
             if (!found){
@@ -59,6 +56,7 @@ fun main(){
 
 
        }
+      var isBothCombo = false
 
 //    Step 4 Calculate Order Total (Loops & Conditions)
         var totalCost = 0.0
@@ -78,7 +76,7 @@ fun main(){
             }
         }
         if(foundItem){
-            println("=> Total cost: $${totalCost}")
+            println("=> Total cost: $${"%.2f".format(totalCost)}")
         }
         if (foundItem){
             if (invalidProduct.isEmpty()){
@@ -89,32 +87,62 @@ fun main(){
             }
         }
 
-    //    Step 5 Apply Discount (Conditional—if-else or when)
-    // countMouse
+//    Step 5 Apply Discount (Conditional—if-else or when)
+//    Laptop + Mouse Combo
      var count = 0
-     if (shoppingCart.any { it.equals("laptop", ignoreCase = true) } && shoppingCart.any { it.equals("mouse", ignoreCase = true) }){
+     var flatDiscount = 10.00
+     var finalCost = 0.0
+     var laptopMouseDis =0.0
+     var headPhoneMicrophoneDis = 0.0
+     var isDiscount = false
+    var discount =0.0
+    if (shoppingCart.any { it.equals("laptop", ignoreCase = true) } && shoppingCart.any { it.equals("mouse", ignoreCase = true) }){
+        isDiscount = true
 
-         for (item in shoppingCart){
-             if (item.lowercase().equals("mouse", ignoreCase = true)){
-                 count += 1
-             }
-         }
+        for (item in shoppingCart){
+            if (item.lowercase().equals("mouse", ignoreCase = true)){
+                count += 1
+            }
+        }
 //         find index of mouse
-         var mouseIndex = productNames.indexOf("Mouse");
+        var mouseIndex = productNames.indexOf("Mouse");
 //         find mouse price
-         var mousePrice = productPrices[mouseIndex]
+        var mousePrice = productPrices[mouseIndex]
 //         find final price of mouse
-         var totalMouse = mousePrice * count
+        var totalMouse = mousePrice * count
 //         8% of total mouse
-         var discount =  totalMouse * 0.08
+         discount =  totalMouse * 0.08
 //         discount with final cost
-          var finalCost = totalCost - discount
-         println("======= Total after discount =======")
-         println("=> Total cost $$finalCost")
+        laptopMouseDis  = totalCost - discount
+        finalCost = laptopMouseDis
 
-     }else{
-         println("No discount")
-     }
+
+
+
+    }
+    //    Headphones + Microphone Combo
+    if (shoppingCart.any {it.equals("Headphones", ignoreCase = true) } && shoppingCart.any { it.equals("Microphone", ignoreCase = true) }){
+        isDiscount = true
+        headPhoneMicrophoneDis = totalCost - flatDiscount
+        finalCost = headPhoneMicrophoneDis
+
+
+    }
+    // both combo
+    if (shoppingCart.any {it.equals("Laptop", ignoreCase = true) } && shoppingCart.any { it.equals("Mouse", ignoreCase = true) && shoppingCart.any { it.equals("Headphones", ignoreCase = true) && shoppingCart.any { it.equals("Microphone", ignoreCase = true)} } }) {
+        isDiscount = true
+        var finalCombo = discount + flatDiscount
+        finalCost  = totalCost - finalCombo
+
+    }
+
+    if (isDiscount) {
+        println("======= Total after discount =======")
+        println("=> Total cost ${"%.2f".format(finalCost)}")
+    }
+
+
+
 
 
 
